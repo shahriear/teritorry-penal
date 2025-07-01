6f9440b80a77f4e9101432a00c2a086d395b791b
shuvo pc <shahriearshuvo@gmail.com>
2025-07-01 12:10:58 +0600

    tt

--- src/components/AdminMenu/Dashboard/SummaryBar.jsx
+++ src/components/AdminMenu/Dashboard/SummaryBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SummaryBar = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 text-center bg-white p-4 md:mt-12 md:mb-11 mt-5 mb-2.5 rounded ">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 text-center bg-white  md:mt-12 md:mb-11 mt-5 mb-6 rounded ">
       {['Master Agent', 'Agent', 'User'].map(type => (
         <div
           key={type}
